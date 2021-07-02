11050c86d18169f403f55a9420872d8e0a7cf2b4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-02 08:39:49 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/ThemleafdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemleafdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemleafdemoApplication.class, args);
+	}
+
+}
